5e29f8fc62f9dbfc50a26e661127787243733ec3
Burcu Noyan <burcunoyan@gmail.com>
2017-08-24 23:13:56 -0400

    set up server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const env = nunjucks.configure('views', { noCache: true });
+
+const app = express();
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+app.use(require('morgan')('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res, next) => {
+  res.send('oh hi');
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`Page ${req.url} not found.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send(err.message || 'Internal error.');
+  next();
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+});
