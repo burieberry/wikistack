2c716860d9d218721911b60e88488c721cbf4de6
Burcu Noyan <burcunoyan@gmail.com>
2017-08-24 23:33:32 -0400

    define models

--- server.js
+++ server.js
@@ -31,7 +31,12 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 3000;
+const db = require('./db');
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}.`);
+  db.sync()
+    .then(() => {
+      console.log(`Listening on port ${port}.`);
+    })
+    .catch(console.error);
 });
