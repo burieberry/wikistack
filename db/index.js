ef4c1f39bd30f5e36d39269aeaaa8bf454612d0e
Burcu Noyan <burcunoyan@gmail.com>
2017-08-25 11:46:29 -0400

    create Page and User modules in db

--- db/index.js
+++ db/index.js
@@ -1,74 +1,7 @@
 const Sequelize = require('sequelize');
-const conn = new Sequelize(process.env.DATABASE_URL, { logging: false });
-
-const Page = conn.define('page', {
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  urlTitle: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true
-  },
-  content: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  },
-  status: {
-    type: Sequelize.STRING,
-    defaultValue: 'open'
-  },
-  route: {
-    type: Sequelize.VIRTUAL,
-    get() {
-      return '/wiki/' + this.getDataValue('urlTitle');
-    }
-  }
-}, {
-  hooks: {
-    beforeValidate: function(page) {
-      if (page.title) {
-        page.urlTitle = page.title.replace(/\s+/g, '_').replace(/\W/g, '').toLowerCase();
-      }
-    }
-  }
-}, {
-  timestamps: false
-});
-
-const User = conn.define('user', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING
-  }
-}, {
-  timestamps: false
-});
-
-User.createUser = function(userData) {
-  return this.create(userData);
-};
-
-User.findUser = function(userData) {
-  return this.findOne({
-    where: {
-      name: userData.name,
-      email: userData.email
-    }
-  })
-};
-
-Page.createPage = function(pageData, user) {
-  return this.create(pageData)
-    .then(page => {
-      if (user) page.setAuthor(user);
-      return page;
-    })
-};
+const conn = require('./conn');
+const Page = require('./Page'),
+      User = require('./User');
 
 Page.belongsTo(User, { as: 'author' });
 
@@ -81,7 +14,6 @@ const seed = () => {
 }
 
 module.exports = {
-  conn,
   sync,
   seed,
   models: {
